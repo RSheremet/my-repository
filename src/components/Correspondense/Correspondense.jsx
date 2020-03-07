db88abe177b0859c274e91f9dbb7343cd6891314
RSheremet <roma.sheremet.91@mail.ru>
2020-03-07 21:42:47 +0300

    1. created DialogItem, Message where dialogs and messages will come to

--- src/components/Correspondense/Correspondense.jsx
+++ src/components/Correspondense/Correspondense.jsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import c from './Correspondense.module.css';
 import {NavLink} from "react-router-dom";
-
-const Dialog = (props) => {
-    let path = "/Correspondense/" + props.id
-    return (
-        <div className={c.item + ' ' + c.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={c.message}>{props.message}</div>
-    )
-}
+import DialogItem from "./DialogItem/DialogsItem";
+import Message from "./Message/Message";
 
 const Correspondense = (props) => {
 
@@ -32,18 +19,16 @@ const Correspondense = (props) => {
         {id: 3, message: "yo"},
     ]
 
+    let Dialogs = conversationData.map( d => <DialogItem name={d.name} id={d.id} /> );
+    let Messages = messagesData.map( m => <Message message={m.message} id={m.id} /> )
+
     return (
         <div className={c.correspondense}>
            <div className={c.wrapper}>
-               <Dialog name={conversationData[0].name} id={conversationData[0].id} />
-               <Dialog name={conversationData[1].name} id={conversationData[1].id} />
-               <Dialog name={conversationData[2].name} id={conversationData[2].id} />
-               <Dialog name={conversationData[3].name} id={conversationData[3].id} />
+               {Dialogs}
            </div>
            <div className={c.messages}>
-               <Message message={messagesData[0].message} id={messagesData[0].id} />
-               <Message message={messagesData[1].message} id={messagesData[1].id} />
-               <Message message={messagesData[2].message} id={messagesData[2].id} />
+               {Messages}
            </div>
         </div>
     )
---
+++ src/components/Correspondense/DialogItem/DialogsItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import c from './../Correspondense.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = "/Correspondense/" + props.id
+    return (
+        <div className={c.item + ' ' + c.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+
+export default DialogItem;
