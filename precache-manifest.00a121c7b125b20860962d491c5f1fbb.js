a85b71231d5cecacaee81a9503c96bc8b7538f91
PoMa <roma.sheremet.91@mail.ru>
2020-05-25 17:39:43 +0400

    Updates

---
+++ precache-manifest.00a121c7b125b20860962d491c5f1fbb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280e5070d026babab05646b0e8f4633e",
+    "url": "/RSheremet/social-network/index.html"
+  },
+  {
+    "revision": "57ade438e0928d8ad0f8",
+    "url": "/RSheremet/social-network/static/css/3.03b90496.chunk.css"
+  },
+  {
+    "revision": "d5d4828c67c1fa0d6f87",
+    "url": "/RSheremet/social-network/static/css/4.41406ed6.chunk.css"
+  },
+  {
+    "revision": "f3c8817e0fda148b0d88",
+    "url": "/RSheremet/social-network/static/css/main.f8a099e5.chunk.css"
+  },
+  {
+    "revision": "a4edd22a7fa0cb5d59c1",
+    "url": "/RSheremet/social-network/static/js/2.ed137468.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/RSheremet/social-network/static/js/2.ed137468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ade438e0928d8ad0f8",
+    "url": "/RSheremet/social-network/static/js/3.7b8b659d.chunk.js"
+  },
+  {
+    "revision": "d5d4828c67c1fa0d6f87",
+    "url": "/RSheremet/social-network/static/js/4.07c1c021.chunk.js"
+  },
+  {
+    "revision": "f3c8817e0fda148b0d88",
+    "url": "/RSheremet/social-network/static/js/main.f9afcb7e.chunk.js"
+  },
+  {
+    "revision": "7142ccda2ef18819cfa1",
+    "url": "/RSheremet/social-network/static/js/runtime-main.e248f9b9.js"
+  },
+  {
+    "revision": "5c7aaf2da54d4ade8c32a34e867f0b5e",
+    "url": "/RSheremet/social-network/static/media/im_working.5c7aaf2d.jpg"
+  },
+  {
+    "revision": "9bf01a7a8a70f6d8ffbfc8b908a4cbfe",
+    "url": "/RSheremet/social-network/static/media/logo.9bf01a7a.jpg"
+  },
+  {
+    "revision": "8bc6336c74abb2df6607b1dd7b0129fb",
+    "url": "/RSheremet/social-network/static/media/looking_for_job.8bc6336c.jpeg"
+  },
+  {
+    "revision": "114a6d05469c0d499644668bca817ed4",
+    "url": "/RSheremet/social-network/static/media/preloader.114a6d05.gif"
+  }
+]);
